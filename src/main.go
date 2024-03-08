ebccd3664cd13ca575e1df26a7444c588031209c
Bayashin <k21099kk@aitech.ac.jp>
2024-03-09 01:54:09 +0900

    update:meinの設定

--- src/main.go
+++ src/main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
+	"github.com/SystemEngineeringTeam/ait-guide-back/controller"
 	"github.com/SystemEngineeringTeam/ait-guide-back/service"
 )
 
 func main() {
-	service.Migration("240212")
-	// for _, v := range model.GetAllPoints() {
-	// 	fmt.Println(v)
-	// }
-	// model.GetClosestPoint(35.18411661111426, 137.1110111474991)
+	service.Migration()
+	controller.ApiHandler()
 }
